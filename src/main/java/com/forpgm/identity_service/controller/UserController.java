86843976db8456eb2e66c1a716b91583ff609521
Forpgm <phm.giamy@gmail.com>
2025-10-04 23:05:54 +0700

    update user

--- src/main/java/com/forpgm/identity_service/controller/UserController.java
+++ src/main/java/com/forpgm/identity_service/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.forpgm.identity_service.controller;
 
 import com.forpgm.identity_service.dto.request.CreateUserRequest;
+import com.forpgm.identity_service.dto.request.UpdateUserRequest;
 import com.forpgm.identity_service.entity.User;
 import com.forpgm.identity_service.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -24,5 +26,15 @@ public class UserController {
     @GetMapping
     List<User> listUsers() {
         return userService.getAllUsers();
+    }
+
+    @GetMapping("/{userId}")
+    User getUser(@PathVariable("userId") String userId) {
+        return userService.getUser(userId);
+    }
+
+    @PutMapping("/{userId}")
+    User updateUser(@PathVariable("userId") String userId,@RequestBody UpdateUserRequest updateUserRequest) {
+        return userService.updateUser(userId,updateUserRequest);
     }
 }
--- src/main/java/com/forpgm/identity_service/service/UserService.java
+++ src/main/java/com/forpgm/identity_service/service/UserService.java
@@ -1,12 +1,15 @@
 package com.forpgm.identity_service.service;
 
 import com.forpgm.identity_service.dto.request.CreateUserRequest;
+import com.forpgm.identity_service.dto.request.UpdateUserRequest;
 import com.forpgm.identity_service.entity.User;
 import com.forpgm.identity_service.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -28,4 +31,19 @@ public class UserService {
    public List<User> getAllUsers(){
         return userRepository.findAll();
     }
+
+    public User getUser(String userId){
+        return userRepository.findById(UUID.fromString(userId))
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public User updateUser(String userId, UpdateUserRequest request) {
+        User existingUser = getUser(userId);
+
+        existingUser.setPassword(request.getPassword());
+        existingUser.setFirstName(request.getFirstName());
+        existingUser.setLastName(request.getLastName());
+        existingUser.setDob(request.getDob());
+            return userRepository.save(existingUser);
+        }
 }
